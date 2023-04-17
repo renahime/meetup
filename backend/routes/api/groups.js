463ca18b0ccc7ea6fae1df0a75aa66b694211bb2
renahime <genuinehex@gmail.com>
2023-04-16 21:07:02 -0500

    get group by id

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -6,14 +6,39 @@ const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { Group, User, GroupImage, Membership, Event, Venue } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { group } = require('console');
 
 
 const router = express.Router();
 
-//get "/groups"
+//get api/groups
 router.get('', async (req,res) => {
     const groups = await Group.findAll();
 
     return res.json({Groups:groups});
 })
+
+//get api/groups/:groupId
+router.get('/:groupId', async(req,res) => {
+  const groupId = await Group.findByPk(req.params.groupId,{
+    include: [
+      {
+        model:GroupImage,
+        attributes: ['id','url','preview']
+      },
+      {
+        model:User,
+        as: 'Organizer',
+        attributes: ['id','firstName','lastName'],
+      },
+      {
+        model:Venue,
+        attributes:['id','groupId','address','city','state','lat','lng'],
+      }
+    ],
+  })
+
+  return res.json(groupId);
+})
+
 module.exports = router;

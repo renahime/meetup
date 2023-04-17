55cba989ade973a95ed4c6756f2e205c6aaf74fe
renahime <genuinehex@gmail.com>
2023-04-16 20:14:02 -0500

    created group router

---
+++ backend/routes/api/groups.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { Group, User, GroupImage, Membership, Event, Venue } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+const router = express.Router();
+
+module.exports = router;

fc10d12a871155158ece41dc5be07336c0fafa24
renahime <genuinehex@gmail.com>
2023-04-19 18:55:43 -0500

    finished event routers with error handling

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -14,6 +14,7 @@ const e = require('express');
 
 const router = express.Router();
 
+//get /events
 router.get("/", async (req,res) => {
   const events = await Event.findAll();
 
@@ -25,6 +26,205 @@ router.get("/", async (req,res) => {
 
   return res.json({Events:eventsList});
 
+})
+
+//get /:eventId
+router.get('/:eventId', async (req,res,next) => {
+  const event = await Event.findByPk(req.params.eventId);
+
+  if(!event){
+    return next({
+      message:"Event couldn't be found",
+    })
+  }
+
+  const payloadEvent = event.toJSON();
+  const group = await event.getGroup({where:event.groupId});
+  const payloadGroup = group.toJSON();
+  delete payloadGroup.organizerId;
+  delete payloadGroup.createdAt;
+  delete payloadGroup.updatedAt;
+  const venue = await event.getVenue({where:event.venueId});
+  const payloadVenue = venue.toJSON();
+  delete payloadVenue.groupId;
+  delete payloadVenue.createdAt;
+  delete payloadVenue.updatedAt;
+  const eventImages = await event.getEventImages({where:event.eventId});
+  const payloadEventImages = eventImages[0].toJSON();
+  delete payloadEventImages.eventId;
+  delete payloadEventImages.createdAt;
+  delete payloadEventImages.updatedAt;
+
+  const payload = {
+    id: payloadEvent.id,
+    groupid: payloadEvent.groupid,
+    venueId: payloadEvent.venueId,
+    name: payloadEvent.name,
+    description: payloadEvent.description,
+    type: payloadEvent.type,
+    capacity: payloadEvent.capacity,
+    price: payloadEvent.price,
+    startDate: payloadEvent.startDate,
+    endDate: payloadEvent.endDate,
+    Group: payloadGroup,
+    Venue: payloadVenue,
+    EventImages: payloadEventImages
+  }
+
+  return res.json(payload);
+})
+
+
+//post /:eventId/images
+router.post('/:eventId/images', async (req,res,next) => {
+  const event = await Event.findByPk(req.params.eventId);
+
+  if(!event){
+    return next({
+      message:"Event couldn't be found",
+    })
+  }
+
+  const {preview, url} = req.body;
+
+  let newImage = await event.createEventImage({
+    eventId: req.params.eventId,
+    url:url,
+    preview:preview
+  })
+
+  newImage = newImage.toJSON();
+  delete newImage.id;
+  delete newImage.eventId;
+  delete newImage.createdAt;
+  delete newImage.updatedAt;
+
+  return res.json(newImage);
+})
+
+//put /:eventId
+router.put('/:eventId', async (req,res,next) => {
+  const event = await Event.findByPk(req.params.eventId);
+
+  if(!event){
+    return next({
+      message:"Event couldn't be found",
+    })
+  }
+
+  const errors = {};
+  const {venueId, name, type, capacity, price, description, startDate, endDate} = req.body;
+
+  if(venueId !== undefined){
+    let checkVenue = await Venue.findByPk(venueId);
+    if(!checkVenue){
+      errors.venueId = "Venue does not exist";
+      next({
+        message:"Bad Request",
+        errors:errors
+      })
+    }
+    event.venueId = venueId;
+  }
+
+  if(name !== undefined){
+    if(name.length < 5){
+      errors.name = "Name must be at least 5 characters";
+      next({
+        message:"Bad Request",
+        errors:errors
+      })
+    }
+    event.name = name;
+  }
+
+  if(type !== undefined){
+    if(type.toLowerCase() !== "virtual" && type.toLowercase() !== "in-person"){
+      errors.type = "Type must be 'Virtual' or 'In-Person'"
+      return next({
+        message:"Bad Request",
+        errors:errors,
+      })
+    }
+    event.type = type;
+  }
+
+  if(capacity !== undefined){
+    if(typeof capacity !== "Integer"){
+      errors.capacity = "Capacity must be an integer";
+      return next({
+        message:"Bad Request",
+        errors:errors
+      })
+    }
+    event.capacity = capacity;
+  }
+
+  if(price !== undefined){
+    if(price < 0){
+      errors.price = "Price is invalid";
+      return next({
+        message:"Bad Request",
+        errors:errors
+      })
+    }
+    event.price = price;
+  }
+
+  if(description !== undefined){
+    if(!description){
+      errors.description = "Description is required"
+      return next({
+        message:"Bad Request",
+        errors:errors
+      })
+    }
+    event.description = description;
+  }
+
+  if(startDate !== undefined){
+    if(startDate <= new Date()){
+      errors.startDate = "Start date must be in the future";
+      return next({
+        message:"Bad Request",
+        errors:errors
+      })
+    }
+    event.startDate = startDate;
+  }
+
+  if(endDate !== undefined){
+    if(endDate < startDate){
+      errors.endDate = "End date is less than start date";
+      return next({
+        message:"Bad Request",
+        errors:errors
+      })
+    }
+    event.endDate = endDate;
+  }
+
+  await event.save();
+  event = event.toJSON();
+  return res.json(event);
+})
+
+//delete /:eventId
+
+router.delete('/:eventId', async (req,res,next) => {
+  const deleteEvent = await Event.findByPk(req.params.eventId);
+
+  if(!deleteEvent){
+    return next({
+      message:"Event couldn't be found",
+    })
+  }
+
+  await deleteEvent.destroy();
+
+  return res.json({
+  message: "Successfully deleted"
+  })
 })
 
 module.exports = router;
--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -246,7 +246,7 @@ router.put('/:groupId', async (req,res,next) => {
 
   if(type !== undefined){
 
-    if(type.toLowerCase() !== "virtual" || type.toLowercase() !== "in-person"){
+    if(type.toLowerCase() !== "virtual" && type.toLowercase() !== "in-person"){
       errors.type = "Type must be 'Virtual' or 'In-Person'"
       return next({
         message:"Bad Request",
@@ -313,10 +313,9 @@ router.delete('/:groupId', async (req, res, next) => {
 
   await deleteGroup.destroy();
 
-  res.json({
+  return res.json({
     message: "Successfully deleted"
-  }
-  )
+  })
 })
 
 //get /:groupId/venues
@@ -412,5 +411,134 @@ router.post('/:groupId/venues', async (req,res,next) => {
   return res.json(newVenue);
 })
 
+//get /:groupId/events
+router.get('/:groupId/events', async (req,res,next) => {
+  let foundEvents = await Event.findAll({
+    where:{
+      groupId:req.params.groupId,
+    },
+    include: [{
+      model:Group,
+      attributes:['id', 'name', 'city', 'state']
+    },
+    {
+      model:Venue,
+      attributes:['id', 'city', 'state']
+    }]
+  })
+
+  let pushedEvents = [];
+
+  foundEvents.forEach(event => {
+    let ele = event.toJSON();
+    delete ele.updatedAt;
+    delete ele.createdAt;
+    pushedEvents.push(ele);
+  })
+
+  if(pushedEvents.length === 0){
+    return next({
+      message:"Group couldn't be found",
+    })
+  }
+
+  return res.json({Events:pushedEvents});
+})
+
+//post /:groupId/events
+router.post('/:groupId/events', async (req,res,next) => {
+  const group = await Group.findByPk(req.params.groupId);
+
+  if(!group){
+    return next({
+      message:"Group couldn't be found",
+    })
+  }
+
+  const errors = {};
+  const {venueId, name, type, capacity, price, description, startDate, endDate} = req.body;
+
+  let checkVenue = await Venue.findByPk(venueId);
+
+  if(!checkVenue){
+    errors.venueId = "Venue does not exist";
+    next({
+      message:"Bad Request",
+      errors:errors
+    })
+  }
+
+  if(name.length < 5){
+    errors.name = "Name must be at least 5 characters";
+    next({
+      message:"Bad Request",
+      errors:errors
+    })
+  }
+
+  if(type.toLowerCase() !== "virtual" && type.toLowercase() !== "in-person"){
+    errors.type = "Type must be 'Virtual' or 'In-Person'"
+    return next({
+      message:"Bad Request",
+      errors:errors,
+    })
+  }
+
+  if(typeof capacity !== "Integer"){
+    errors.capacity = "Capacity must be an integer";
+    return next({
+      message:"Bad Request",
+      errors:errors
+    })
+  }
+
+  if(price < 0){
+    errors.price = "Price is invalid";
+    return next({
+      message:"Bad Request",
+      errors:errors
+    })
+  }
+
+  if(!description){
+    errors.description = "Description is required"
+    return next({
+      message:"Bad Request",
+      errors:errors
+    })
+  }
+
+  if(startDate <= new Date()){
+    errors.startDate = "Start date must be in the future";
+    return next({
+      message:"Bad Request",
+      errors:errors
+    })
+  }
+
+  if(endDate < startDate){
+    errors.endDate = "End date is less than start date";
+    return next({
+      message:"Bad Request",
+      errors:errors
+    })
+  }
+
+
+  let newEvent = await group.createEvent({
+    venueId:venueId,
+    name:name,
+    type:type,
+    capacity:capacity,
+    price:price,
+    description:description,
+    startDate:startDate,
+    endDate:endDate
+  })
+
+  newEvent = newEvent.toJSON();
+
+  return res.json(newEvent);
+})
 
 module.exports = router;
